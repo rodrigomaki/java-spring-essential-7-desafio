c0235d848716e0a4c8eba208d15e9591dc643b10
Rodrigo Maki <rodrigomaki@gmail.com>
2023-10-28 22:59:01 -0300

    add Atividade

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Atividade.java
@@ -0,0 +1,74 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private Double price;
+
+    public Atividade() {
+    }
+
+    public Atividade(Integer id, String nome, String descricao, Double price) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Atividade atividade = (Atividade) o;
+
+        return getId() != null ? getId().equals(atividade.getId()) : atividade.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
