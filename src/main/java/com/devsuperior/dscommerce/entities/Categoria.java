fa5cc25eee59eb8443c96c1d794c0522c271761e
Rodrigo Maki <rodrigomaki@gmail.com>
2023-10-28 22:45:39 -0300

    add Categoria

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Categoria.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return getId() != null ? getId().equals(categoria.getId()) : categoria.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
